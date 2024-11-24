a4efbb8e33dc314ce3bb3a9ddfc97eecc9720054
KanYouNot <ostrowkakacper@gmail.com>
2024-11-24 23:17:27 +0100

    updated the calculator

--- micro-projects/PromptCalculator.java
+++ micro-projects/PromptCalculator.java
@@ -5,16 +5,16 @@ class PromptCalculator{
 	
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter the first number:");
-		double n1 = input.nextDouble();
+		int n1 = input.nextInt();
 		
 		System.out.println("Enter the second number:");
-		double n2 = input.nextDouble();
+		int n2 = input.nextInt();
 		input.nextLine();
 
 		System.out.println("Enter the operation:  + - * /");
 		String op = input.nextLine();
 
-		double ou;
+		int ou = 0;
 
 		switch(op){
 			case "+":
@@ -30,8 +30,11 @@ class PromptCalculator{
 				System.out.println("The result is "+ ou);
 				break;
 			case "/":
-				ou = n1/n2;
-				System.out.println("The result is "+ ou);
+        double nd1 = n1;
+        double nd2 = n2;
+				double oud = ou;
+        oud = nd1/nd2;
+				System.out.println("The result is "+ oud);
 				break;
 			default:
 				System.out.println("Enter the right things dummy!");
