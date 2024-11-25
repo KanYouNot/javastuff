2bc586eda796ad5cccff1d59c09392d85d75e41f
KanYouNot <ostrowkakacper@gmail.com>
2024-11-25 16:10:34 +0100

    fixed diceroller

--- micro-projects/DiceRoller.java
+++ micro-projects/DiceRoller.java
@@ -1,12 +1,12 @@
 public class DiceRoller{
   public static void main(String[] args){
     int d6 = (int)(Math.random()*6)+1;
-    System.out.println("Rolled D6:"+d6);
-    int d6 = (int)(Math.random()*20)+1;
+    System.out.println("Rolled D6: "+d6);
+    int d20 = (int)(Math.random()*20)+1;
     System.out.println("Rolled D20: "+d20);
-    int d6 = (int)(Math.random()*8)+1;
+    int d8 = (int)(Math.random()*8)+1;
     System.out.println("Rolled D8: "+d8);
-    int d6 = (int)(Math.random()*4)+1;
+    int d4 = (int)(Math.random()*4)+1;
     System.out.println("Rolled D4: "+d4);
   }
 }
